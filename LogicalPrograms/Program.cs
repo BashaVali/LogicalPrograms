5b39f4fd6f382dce7259b93ea04e1218d461e77a
BashaVali <bashavali222@gmail.com>
2022-11-02 10:01:14 +0530

    Logical Programs

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using LogicalPrograms;
+using System;
+
+namespace DemoExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LogicalPrograms" + "\n\n");
+            Console.WriteLine("Enter the option");
+            Console.WriteLine("1.FibonacciSeries" + "\n");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    Console.WriteLine("\n\n" + "Enter the FibonacciSeries Value" + "\n");
+                    int range = Convert.ToInt32(Console.ReadLine());
+                    series.Print(range);
+                    break;
+            }
+        }
+    }
+}
