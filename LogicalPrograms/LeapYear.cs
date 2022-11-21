0e2b2db0d01d7188b2e6bdd1ef03c543e16b25c8
BashaVali <bashavali222@gmail.com>
2022-11-22 04:16:31 +0530

    Logical Programs

---
+++ LogicalPrograms/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leapyear
+{
+    public class LeapYear
+    {
+        public void Leap()
+        {
+            int year;
+
+            Console.WriteLine("Enter The Year ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("The " + year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("The " + year + " is not a leap year");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,4 +1,5 @@
-﻿using LogicalProblems;
+﻿using Leapyear;
+using LogicalProblems;
 using LogicalPrograms;
 using System;
 
@@ -7,79 +8,95 @@ namespace LogicalProblems
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("LogicalProblems" + "\n\n");
-            Console.WriteLine("Select the option");
-            Console.WriteLine("1.FibonacciSeries" + "\n" +
-                                "2.PerfectNumber" + "\n" +
-                                "3.PrimeNumber" + "\n" +
-                                "4.ReverseNumber" + "\n" +
-                                "5.CouponNumber" + "\n" +
-                                "6.TemperatureConversion" + "\n"+
-                                "7.StopWatch" + "\n"+
-                                "8.VendingMachine" + "\n"+
-                                "9.MonthlyPayment" + "\n");
-            int options = Convert.ToInt32(Console.ReadLine());
-            switch (options)
+        { 
+            Console.WriteLine("Welcome to the Logical Programs");
+            bool flag = true;
+            while (flag)
             {
-                case 1:
-                    FibonacciSeries series = new FibonacciSeries();
-                    Console.WriteLine("\n\n" + "Enter the FibonacciSeries Value" + "\n");
-                    int range = Convert.ToInt32(Console.ReadLine());
-                    series.Print(range);
-                    break;
-                case 2:
-                    PerfectNumber perfectNumber = new PerfectNumber();
-                    Console.WriteLine("\n\n" + "Check the Number Perfect or Not" + "\n");
-                    Console.WriteLine("Enter the Value" + "\n\n");
-                    int value = Convert.ToInt32(Console.ReadLine());
-                    perfectNumber.Check(value);
-                    break;
-                case 3:
-                    PrimeNumber primeNumber = new PrimeNumber();
-                    Console.WriteLine("Enter the Value to check Prime" + "\n\n");
-                    int value1 = Convert.ToInt32(Console.ReadLine());
-                    primeNumber.CheckPrime(value1);
-                    break;
-                case 4:
-                    ReverseNumber reverseNumber = new ReverseNumber();
-                    Console.WriteLine("Enter the Value to Reverse" + "\n\n");
-                    int value2 = Convert.ToInt32(Console.ReadLine());
-                    reverseNumber.Reverse(value2);
-                    break;
-                case 5:
-                    CouponNumber couponNumber = new CouponNumber();
-                    Console.WriteLine("\n" + "Enter the Coupon Number" + "\n");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    CouponNumber.GenerateCoupons(num);
-                    break;
-                case 6:
-                    TemperatureConversion temperatureConversion = new TemperatureConversion();
-                    Console.WriteLine("1. Celcius to Fahrenheit  \n 2.  Fahrenheit to celcius ");
-                    int TemperatureConversion = Convert.ToInt32(Console.ReadLine());
-                    if (TemperatureConversion == 0)
-                    {
-                        float f = temperatureConversion.celcuisConvert(5);
-                        Console.WriteLine("\n" + "Celcius to Fahrenheit  is " + f);
-                    }
-                    else
-                    {
-                        float c = temperatureConversion.fahrenhiteConvert(5);
-                        Console.WriteLine("\n" + "Fahrenheit to celcius is " + c);
-                    }
-                    break;
-                case 7:
-                    StopWatch stopWatch = new StopWatch();
-                    stopWatch.Start();
-                    break;
-                case 8:
-                    VendingMachine vendingMachine = new VendingMachine();
-                    vendingMachine.Notes(2250);                     
-                    break;
-                case 9:
-                    MonthlyPayments monthlyPayments = new MonthlyPayments();
-                    MonthlyPayments.MonthlyPayment(10,20,30);
-                    break;
+                Console.WriteLine("LogicalProblems" + "\n\n");
+                Console.WriteLine("Select the option");
+                Console.WriteLine("1.FibonacciSeries" + "\n" +
+                                    "2.PerfectNumber" + "\n" +
+                                    "3.PrimeNumber" + "\n" +
+                                    "4.ReverseNumber" + "\n" +
+                                    "5.CouponNumber" + "\n" +
+                                    "6.TemperatureConversion" + "\n" +
+                                    "7.StopWatch" + "\n" +
+                                    "8.VendingMachine" + "\n" +
+                                    "9.MonthlyPayment" + "\n" +
+                                    "10.LeapYear" + "\n" +
+                                    "11.Exit" + "\n");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        Console.WriteLine("\n\n" + "Enter the FibonacciSeries Value" + "\n");
+                        int range = Convert.ToInt32(Console.ReadLine());
+                        series.Print(range);
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        Console.WriteLine("\n\n" + "Check the Number Perfect or Not" + "\n");
+                        Console.WriteLine("Enter the Value" + "\n\n");
+                        int value = Convert.ToInt32(Console.ReadLine());
+                        perfectNumber.Check(value);
+                        break;
+                    case 3:
+                        PrimeNumber primeNumber = new PrimeNumber();
+                        Console.WriteLine("Enter the Value to check Prime" + "\n\n");
+                        int value1 = Convert.ToInt32(Console.ReadLine());
+                        primeNumber.CheckPrime(value1);
+                        break;
+                    case 4:
+                        ReverseNumber reverseNumber = new ReverseNumber();
+                        Console.WriteLine("Enter the Value to Reverse" + "\n\n");
+                        int value2 = Convert.ToInt32(Console.ReadLine());
+                        reverseNumber.Reverse(value2);
+                        break;
+                    case 5:
+                        CouponNumber couponNumber = new CouponNumber();
+                        Console.WriteLine("\n" + "Enter the Coupon Number" + "\n");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        CouponNumber.GenerateCoupons(num);
+                        break;
+                    case 6:
+                        TemperatureConversion temperatureConversion = new TemperatureConversion();
+                        Console.WriteLine("1. Celcius to Fahrenheit  \n 2.  Fahrenheit to celcius ");
+                        int TemperatureConversion = Convert.ToInt32(Console.ReadLine());
+                        if (TemperatureConversion == 0)
+                        {
+                            float f = temperatureConversion.celcuisConvert(5);
+                            Console.WriteLine("\n" + "Celcius to Fahrenheit  is " + f);
+                        }
+                        else
+                        {
+                            float c = temperatureConversion.fahrenhiteConvert(5);
+                            Console.WriteLine("\n" + "Fahrenheit to celcius is " + c);
+                        }
+                        break;
+                    case 7:
+                        StopWatch stopWatch = new StopWatch();
+                        stopWatch.Start();
+                        break;
+                    case 8:
+                        VendingMachine vendingMachine = new VendingMachine();
+                        vendingMachine.Notes(2250);
+                        break;
+                    case 9:
+                        MonthlyPayments monthlyPayments = new MonthlyPayments();
+                        MonthlyPayments.MonthlyPayment(10, 20, 30);
+                        break;
+                        case 10:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.Leap();
+                        break;
+                    case 11:
+                        flag = false;
+                        break;
+
+
+                }
 
             }
 
